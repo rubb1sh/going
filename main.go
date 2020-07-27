75679922125b789b51c0a66e6b194857899f1ef7
Junshu.Mou <junshu.mou@shopee.com>
2020-07-27 21:04:01 +0800

    1

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"reflect"
+)
 
+type CustomError struct{}
+
+func (*CustomError) Error() string {
+	return ""
+}
+
+func main() {
+	typeOfError := reflect.TypeOf((*error)(nil)).Elem()
+	customErrorPtr := reflect.TypeOf(&CustomError{})
+	customError := reflect.TypeOf(CustomError{})
+
+	fmt.Println(customErrorPtr.Implements(typeOfError)) // #=> true
+	fmt.Println(customError.Implements(typeOfError))    // #=> false
 }
